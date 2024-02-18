9a49e54579a6a990933401e69c2e677327bc03c6
liamlup <89188072+liamlup@users.noreply.github.com>
2024-02-18 13:24:18 +0200

    Batch support

--- example.py
+++ example.py
@@ -1,11 +1,19 @@
 from foveate import FoveateImage
 from torchvision.io import read_image
 import matplotlib.pyplot as plt
+from random import randint
 
 if __name__ == '__main__':
     img = read_image("data/face.jpg")
+    img_batch = img.repeat(16, 1, 1, 1)
     xc, yc = int(img.shape[2] / 2), int(img.shape[1] / 2)
-    foveate = FoveateImage()(img, [(xc, yc)])
-    plt.imshow(foveate.permute(1, 2, 0))
-    plt.scatter(xc, yc, c='r', s=50, marker='x')
+    fixations = [(randint(0, img.shape[2]), randint(0, img.shape[1])) for _ in range(16)]
+
+    foveate = FoveateImage()(img_batch, fixations)
+
+    fig, ax = plt.subplots(4, 4, figsize=(10, 10))
+    for i in range(4):
+        for j in range(4):
+            ax[i, j].imshow(foveate[i * 4 + j].permute(1, 2, 0))
+            ax[i, j].scatter(fixations[i * 4 + j][0], fixations[i * 4 + j][1], c='r', s=30, marker='x')
     plt.show()
--- foveate.py
+++ foveate.py
@@ -25,19 +25,18 @@ def genGaussiankernel(width, sigma):
     return kernel_2d
 
 
-def pyramid(img, sigma: torch.Tensor = 1, prNum=6):
-    im_tensor = img.clone().float()
-    height_ori, width_ori = im_tensor.shape[1], im_tensor.shape[2]
-    G = im_tensor.clone()
+def pyramid(imgs, sigma: torch.Tensor = 1, prNum=6):
+    batch_size, channels, height_ori, width_ori = imgs.shape
+    G = imgs.clone().float()
     pyramids = [G]
 
     Gaus_kernel2D = genGaussiankernel(5, sigma)
-    Gaus_kernel2D = Gaus_kernel2D.repeat(3, 1, 1, 1)  # Adjusting for 3 channels
+    Gaus_kernel2D = Gaus_kernel2D.repeat(channels, 1, 1, 1)  # Adjusting for channels
 
     # Down sample
     for i in range(1, prNum):
-        G = F.conv2d(G.unsqueeze(0), Gaus_kernel2D, padding=2, groups=3)
-        G = F.interpolate(G, scale_factor=(0.5, 0.5), mode='bilinear', align_corners=False).squeeze(0)
+        G = F.conv2d(G, Gaus_kernel2D, padding=2, groups=channels)
+        G = F.interpolate(G, scale_factor=(0.5, 0.5), mode='bilinear', align_corners=False)
         pyramids.append(G)
 
     # Up sample
@@ -45,30 +44,31 @@ def pyramid(img, sigma: torch.Tensor = 1, prNum=6):
         curr_im = pyramids[i]
         for j in range(i):
             if j < i - 1:
-                im_size = (curr_im.shape[1] * 2, curr_im.shape[2] * 2)
+                im_size = (curr_im.shape[2] * 2, curr_im.shape[3] * 2)
             else:
                 im_size = (height_ori, width_ori)
-            curr_im = F.interpolate(curr_im.unsqueeze(0), size=im_size, mode='bilinear', align_corners=False).squeeze(0)
-            curr_im = F.conv2d(curr_im.unsqueeze(0), Gaus_kernel2D, padding=2, groups=3).squeeze(0)
+            curr_im = F.interpolate(curr_im, size=im_size, mode='bilinear', align_corners=False)
+            curr_im = F.conv2d(curr_im, Gaus_kernel2D, padding=2, groups=channels)
 
         pyramids[i] = curr_im
 
-    return torch.stack(pyramids, dim=0)
+    return torch.stack(pyramids, dim=1)
 
 
-def foveate_image(img, fixs):
+def foveate_image(img_batch, fixs_batch):
     """
-    img: input image
-    fixs: sequences of fixations of form [(x1, y1), (x2, y2), ...]
+        img_batch: input batch of images [B, C, H, W]
+        fixs_batch: list of fixations for each image in the batch [(x1, y1), (x2, y2), ...]
 
-    This function outputs the foveated image with given input image and fixations.
-    """
+        This function outputs the foveated images for the given batch of images and fixations.
+        """
     # TODO: Use visual angle instead of pixels for fixations
+    # TODO: resize to same size
 
     sigma = torch.tensor(0.248)
     prNum = 6
-    As = pyramid(img, sigma, prNum)
-    _, height, width = img.shape
+    As = pyramid(img_batch, sigma, prNum)
+    batch_size, _, height, width = img_batch.shape
 
     # compute coefficients
     p = torch.tensor(7.5)
@@ -79,50 +79,54 @@ def foveate_image(img, fixs):
     y = torch.arange(0, height, dtype=torch.float32)
     x2d, y2d = torch.meshgrid(x, y, indexing='xy')
 
-    fixs_tensor = torch.tensor(fixs)  # Convert fixations to a tensor
-    distances = torch.sqrt((x2d.unsqueeze(-1) - fixs_tensor[:, 0]) ** 2 + (y2d.unsqueeze(-1) - fixs_tensor[:, 1]) ** 2)
-    theta = torch.min(distances, dim=-1).values / p
-    R = alpha / (theta + alpha)
+    im_fov_batch = []
 
-    levels = torch.arange(1, prNum, dtype=torch.float32)
-    Ts = torch.exp(-((2 ** (levels.view(-1, 1, 1) - 3)) * R.unsqueeze(0) / sigma) ** 2 * k)
-    Ts = torch.cat((Ts, torch.zeros(1, R.shape[0], R.shape[1])), dim=0)
+    for b in range(batch_size):
+        fixs = fixs_batch[b]
+        fixs_tensor = torch.tensor(fixs, dtype=torch.float32).unsqueeze(0)  # Convert fixations to a tensor for each image
+        distances = torch.sqrt(
+            (x2d.unsqueeze(-1) - fixs_tensor[:, 0]) ** 2 + (y2d.unsqueeze(-1) - fixs_tensor[:, 1]) ** 2)
+        theta = torch.min(distances, dim=-1).values / p
+        R = alpha / (theta + alpha)
 
-    # Omega
-    omega = torch.sqrt(torch.log(torch.tensor(2.0)) / k) / (2 ** (levels - 3)) * sigma
-    omega = torch.cat((omega, torch.tensor([0.])), dim=0)
-    omega[omega > 1] = 1
+        levels = torch.arange(1, prNum, dtype=torch.float32)
+        Ts = torch.exp(-((2 ** (levels.view(-1, 1, 1) - 3)) * R.unsqueeze(0) / sigma) ** 2 * k)
+        Ts = torch.cat((Ts, torch.zeros(1, R.shape[0], R.shape[1])), dim=0)
 
-    # Layer index
-    layer_ind = torch.zeros_like(R)
-    for i in range(1, prNum):
-        ind = torch.logical_and(torch.ge(R, omega[i]), torch.le(R, omega[i - 1]))
-        layer_ind[ind] = i
+        # Omega
+        omega = torch.sqrt(torch.log(torch.tensor(2.0)) / k) / (2 ** (levels - 3)) * sigma
+        omega = torch.cat((omega, torch.tensor([0.])), dim=0)
+        omega[omega > 1] = 1
 
-    # B
-    Bs = [(0.5 - Ts[i]) / (Ts[i - 1] - Ts[i] + 1e-5) for i in range(1, prNum)]
+        # Layer index
+        layer_ind = torch.zeros_like(R)
+        for i in range(1, prNum):
+            ind = torch.logical_and(torch.ge(R, omega[i]), torch.le(R, omega[i - 1]))
+            layer_ind[ind] = i
 
-    # M
-    Ms = torch.zeros((prNum, R.shape[0], R.shape[1]))
+        # B
+        Bs = [(0.5 - Ts[i]) / (Ts[i - 1] - Ts[i] + 1e-5) for i in range(1, prNum)]
 
-    for i in range(prNum):
-        ind = torch.isclose(layer_ind, torch.tensor(i, dtype=torch.float32))
-        if torch.sum(ind) > 0:
-            if i == 0:
-                Ms[i][ind] = 1
-            else:
-                Ms[i][ind] = 1 - Bs[i - 1][ind]
+        # M
+        Ms = torch.zeros((prNum, R.shape[0], R.shape[1]))
 
-        ind = torch.isclose(layer_ind - 1, torch.tensor(i, dtype=torch.float32))
-        if torch.sum(ind) > 0:
-            Ms[i][ind] = Bs[i][ind]
+        for i in range(prNum):
+            ind = torch.isclose(layer_ind, torch.tensor(i, dtype=torch.float32))
+            if torch.sum(ind) > 0:
+                if i == 0:
+                    Ms[i][ind] = 1
+                else:
+                    Ms[i][ind] = 1 - Bs[i - 1][ind]
 
-    print('num of full-res pixel', torch.sum(torch.isclose(Ms[0], torch.tensor(1.))))
+            ind = torch.isclose(layer_ind - 1, torch.tensor(i, dtype=torch.float32))
+            if torch.sum(ind) > 0:
+                Ms[i][ind] = Bs[i][ind]
 
-    # generate periphery image
-    im_fov = (As * Ms.unsqueeze(1)).sum(dim=0)
+        # generate periphery image for each image in the batch
+        im_fov = (As[b] * Ms.unsqueeze(1)).sum(dim=0)
+        im_fov_batch.append(im_fov)
 
-    return im_fov.to(torch.uint8)
+    return torch.stack(im_fov_batch, dim=0) / 255.0
 
 
 class FoveateImage(torch.nn.Module):
